84af40dfe62b1f62546c80f3e76984dca37aed93
Kat Michaela <k@gothfem.me>
2018-12-06 13:44:15 -0500

    goodbye url :)

--- src/ChannelPage/VideoContainer.js
+++ src/ChannelPage/VideoContainer.js
@@ -3,25 +3,26 @@ import { Segment } from 'semantic-ui-react';
 import VideoPlayer from './VideoPlayer';
 
 const VideoContainer = ({ myStream, currentUsers, myUsername }) => {
-  let myVideo;
-  const setRef = (el) => {
-    myVideo = el
-    if (myVideo) {
-      myVideo.srcObject = myStream.stream
-    }
-  }
+  // let myVideo;
+  // const setRef = (el) => {
+  //   myVideo = el
+  //   if (myVideo) {
+  //     myVideo.srcObject = myStream.stream
+  //   }
+  // }
   return (
     <Segment inverted style={{ borderRadius:"0", display:"flex", margin:"0", flexDirection:"row", flexBasis:"auto", minHeight:"50vh", flexWrap:"wrap", alignItems:"center", alignContent: "center", justifyContent:"center", boxShadow:"inset 0 0 5px 1px rgb(0, 0, 0, 0.5)"}}>
     {myStream.stream &&
-      <div style={{textAlign:"center", padding:"1rem", flex:"1 1 20%", maxWidth:"36vw", minWidth:"10vw"}}>
-        <div style={{position:"relative"}}>
-          <video muted ref={setRef} autoPlay={true} style={{borderRadius:"1%", boxShadow:"0 1px 5px #000", width:"100%"}}>
-          </video>
-        </div>
-        <div>{myUsername}</div>
-      </div>
+      // <div style={{textAlign:"center", padding:"1rem", flex:"1 1 20%", maxWidth:"36vw", minWidth:"10vw"}}>
+      //   <div style={{position:"relative"}}>
+      //     <video muted ref={setRef} autoPlay={true} style={{borderRadius:"1%", boxShadow:"0 1px 5px #000", width:"100%"}}>
+      //     </video>
+      //   </div>
+      //   <div>{myUsername}</div>
+      // </div>
+      <VideoPlayer isMe={true} stream={myStream.stream} username={myUsername}/>
     }
-  {currentUsers.filter(u => u.stream).map(p => <VideoPlayer key={p.id} stream={p.stream} username={p.username}/>)}
+  {currentUsers.filter(u => u.stream).map(p => <VideoPlayer key={p.id} isMe={false} stream={p.stream} username={p.username}/>)}
   </Segment>
   )
 };
--- src/ChannelPage/VideoPlayer.js
+++ src/ChannelPage/VideoPlayer.js
@@ -23,11 +23,11 @@ class VideoPlayer extends Component {
     return (
       <div style={{textAlign:"center", padding:"1rem", flex:"1 1 20%", maxWidth:"36vw", minWidth:"10vw"}}>
           <div style={{position:"relative"}}>
-            <video muted={this.state.isMuted} autoPlay={true} ref={this.setRef} style={{borderRadius:"1%", boxShadow:"0 1px 5px #000", width:"100%"}}>
+            <video muted={this.props.isMe || this.state.isMuted} autoPlay={true} ref={this.setRef} style={{borderRadius:"1%", boxShadow:"0 1px 5px #000", width:"100%"}}>
             </video>
-            <div style={{position:"absolute", top:"0", paddingTop:".5rem", paddingLeft:".5rem"}}>
+            {!this.props.isMe && <div style={{position:"absolute", top:"0", paddingTop:".5rem", paddingLeft:".5rem"}}>
               <Icon onClick={this.muteToggle} style={{cursor:"pointer", textShadow:"0 0 2px rgb(0,0,0,0.5)"}} size="big" name={this.state.isMuted ? "volume off" : "volume up"} />
-            </div>
+            </div>}
           </div>
           <div>{this.props.username}</div>
       </div>
